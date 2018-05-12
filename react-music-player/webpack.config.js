febab0a765f373c5557c131239775a77b9267872
artinhuang <artinhuangcn@gmail.com>
2018-05-12 21:07:34 +0800

    update

--- react-music-player/webpack.config.js
+++ react-music-player/webpack.config.js
@@ -1,10 +1,33 @@
-var path = require('path');
+const path = require('path');
 
-module.exports = {
+const config = {
     mode: 'production',
     entry: './app/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
-}
+    },
+    module: { 
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
+                }
+              }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader',
+            },
+          ]
+        }
+    };
+
+module.exports = config;
